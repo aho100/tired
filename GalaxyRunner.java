ce85adc5e70b76c2ebd3a580101d3406bc71901d
ykato00 <66575793+ykato00@users.noreply.github.com>
2020-06-08 02:08:25 -0400

    Add files via upload

--- GalaxyRunner.java
+++ GalaxyRunner.java
@@ -6,6 +6,9 @@ import info.gridworld.actor.Flower;
 import info.gridworld.actor.Critter;
 import info.gridworld.actor.ExLives;
 import info.gridworld.actor.Ammo;
+import info.gridworld.actor.BulletSupply;
+import info.gridworld.actor.Alien;
+import info.gridworld.actor.Player;
 
 
 
@@ -18,7 +21,7 @@ public class GalaxyRunner{
 		Location goal = new Location(11,11);
 		Player player = new Player(goal);
 		world.playerLocation(player.getLocation());
-		
+
 		world.add(new Location(0,0), player);
 		for (int i = 0; i < 9; i++)
 		{
@@ -32,23 +35,23 @@ public class GalaxyRunner{
 			double y = Math.random() * 12;
 			world.add(new Location ((int)w, (int)y), new Alien(true, player));
 		}
-		
+
 		for (int j =0; j < 3; j++)
 		{
 			world.add(new Ammo());
 		}
-	
-		
-		
+
+
+
 		BulletSupply tracker1 = new BulletSupply(); //tracks bullets and lives on the side of the grid
 		BulletSupply tracker2 = new BulletSupply();
 		BulletSupply tracker3 = new BulletSupply();
 		BulletSupply tracker4 = new BulletSupply();
 		BulletSupply tracker5 = new BulletSupply();
-		
+
 		ExLives life1 = new ExLives();
-		ExLives life2 = new ExLives(); 
-		
+		ExLives life2 = new ExLives();
+
 		world.add(new Location (1,13),life1);
 		world.add(new Location (2,13),life2);
 		world.add(new Location (1,14), tracker1);
@@ -56,15 +59,15 @@ public class GalaxyRunner{
 		world.add(new Location (3,14), tracker3);
 		world.add(new Location (4,14), tracker4);
 		world.add(new Location (5,14), tracker5);
-		
-		
+
+
 		LastTile gg = new LastTile();    //represents the exit
 		world.add(new Location(11,11),gg);
-		
+
 		for (int r = 0; r < 12; r++)
 		{
 			world.add(new Location(r, 12),new Rock());
-		
+
 		}
 		world.add(new Location(0,13), new Rock());
 		world.add(new Location(0,14), new Rock());
@@ -76,9 +79,9 @@ public class GalaxyRunner{
 		{
 			world.add(new Location(q,14),new Rock());
 		}
-		
-		world.playerLocation(player.getLocation()); 
-	
+
+		world.playerLocation(player.getLocation());
+
 		world.show();
 	}
 }
