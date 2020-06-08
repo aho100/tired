8d6a52dd6954c357482540c041eacf3e50a4f829
ykato00 <66575793+ykato00@users.noreply.github.com>
2020-06-07 21:54:32 -0400

    Update Bullet.java

--- info/gridworld/actor/Bullet.java
+++ info/gridworld/actor/Bullet.java
@@ -1,11 +1,6 @@
 
-import info.gridworld.actor.Bug;
-import info.gridworld.actor.Actor;
-import info.gridworld.grid.Location;
-import info.gridworld.grid.Grid;
-import info.gridworld.world.World;
-import info.gridworld.actor.Rock;
-import info.gridworld.actor.Flower;
+package info.gridworld.actor;
+
 import info.gridworld.grid.Grid;
 import info.gridworld.grid.Location;
 
