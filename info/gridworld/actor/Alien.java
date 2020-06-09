c40a39d96a9165c05994807570c6c3ac73ac1b73
Catherine Yu <36079215+cyu00@users.noreply.github.com>
2020-06-08 23:05:10 -0400

    Update Alien.java

--- info/gridworld/actor/Alien.java
+++ info/gridworld/actor/Alien.java
@@ -9,14 +9,14 @@ import java.awt.Color;
 
 public class Alien extends Critter {
 	public boolean boss;
-	public Player player;
+	public Player player; 
 	public int steps;
 	public final Location FIRST_LIFE = new Location(2, 13);
 	public final Location SECOND_LIFE = new Location(1, 13);
 
 	//Constructs an Alien
     //@param rocks: determines if the Alien can place rocks or not
-    public Alien(boolean canPlaceRocks, Player player1) {
+    public Alien(boolean canPlaceRocks, Player player1) { 
     	setColor(Color.GREEN);
     	boss = canPlaceRocks;
     	player = player1;
@@ -36,13 +36,13 @@ public class Alien extends Critter {
     }
 
     //Implemented to kill (i.e. remove) the Player and remove a life from the Player if they are killed
-    //If the Alien kills the Player, it will be removed from the grid.
+    //If the Alien kills the Player, it will be removed from the grid. 
 	//@param actors: the actors to be processed
 	public void processActors(ArrayList<Actor> actors) {
 		Grid<Actor> gr = getGrid();
 
 		for (Actor a : actors) {
-            if (a instanceof Player) {
+            if (a instanceof Player) { 
             	removeSelfFromGrid();
 	        if (gr.get(FIRST_LIFE) != null)
 	            gr.remove(FIRST_LIFE);
@@ -50,15 +50,19 @@ public class Alien extends Critter {
 	            gr.remove(SECOND_LIFE);
 	        //The Player loses the third time they die and "Game Over" is shown on the middle of the screen.
             else
-		    	gr.put(new Location(5,5), new GameOver());
+                for (int y=0; y<12; y++) {
+                    for (int x=0; x<12; x++) {
+                        gr.put(new Location(y,x), new GameOver());
+                    }
+                }
         	}
         }
     }
 
     //Selects the location for the next move
-    /*
+    /* 
       If the Alien did not eat a Player, there is a 50% chance the Alien moves on its next turn; if
-      the Alien moves, it will move one step towards the Player.
+      the Alien moves, it will move one step towards the Player. 
     */
     //@param locs: the possible locations for the next move
     public Location selectMoveLocation(ArrayList<Location> locs) {
@@ -81,7 +85,7 @@ public class Alien extends Critter {
         steps++;
 
         if ((boss) && (steps % 6 == 0)) {
-        	ArrayList<Location> moveLocs = getMoveLocations();
+        	ArrayList<Location> moveLocs = gr.getEmptyAdjacentLocations(getLocation());
             int r = (int) (Math.random() * moveLocs.size());
             Rock rock = new Rock();
             rock.putSelfInGrid(gr, moveLocs.get(r));
@@ -99,7 +103,7 @@ public class Alien extends Critter {
     private boolean chanceFunc() {
 	    Random r = new Random();
 	    int chance = r.nextInt(2);
-	    if (chance == 1)
+	    if (chance == 1) 
 	    	return true;
 	    return false;
     }
