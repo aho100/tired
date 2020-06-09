73478797fafafbefd4401d90f99e287572bf336b
Catherine Yu <36079215+cyu00@users.noreply.github.com>
2020-06-08 22:35:57 -0400

    Update BulletSupply.java

--- info/gridworld/actor/BulletSupply.java
+++ info/gridworld/actor/BulletSupply.java
@@ -1,8 +1,5 @@
 package info.gridworld.actor;
 
-import info.gridworld.grid.Grid;
-import info.gridworld.grid.Location;
-import java.awt.Color;
 import java.awt.Color;
 
 public class BulletSupply extends Actor{
