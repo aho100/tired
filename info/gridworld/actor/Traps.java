1a68f2e67602c2293ccd6210dab9b31b7392cb23
aho100 <66576166+aho100@users.noreply.github.com>
2020-06-09 00:13:30 -0400

    Add files via upload

--- info/gridworld/actor/Traps.java
+++ info/gridworld/actor/Traps.java
@@ -1,21 +1,25 @@
-package info.gridworld.actor;
-
-import info.gridworld.grid.Grid;
+import info.gridworld.actor.Bug;
+import info.gridworld.actor.Actor;
 import info.gridworld.grid.Location;
+import info.gridworld.grid.Grid;
+import info.gridworld.world.World;
+import info.gridworld.actor.Rock;
+import info.gridworld.actor.Flower;
 
 import java.awt.Color;
 
 
 public class Traps extends Actor
 {
-
 	public Traps()
 	{
-		setColor(Color.BLUE);
+		setColor(Color.YELLOW);
 	}
-
-	public void act() 
-	{
-
+	
+	public void act()
+	{	
+		
 	}
+		
 }
+	
